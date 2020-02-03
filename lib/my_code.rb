09f83115fbf82842d3ad93bbfae4674e6ee6bb0a
John Souza <souzajohnlee@gmail.com>
2020-02-03 21:26:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ end
 
 def reduce(array, value=0)
   i = 0
-  array[0].kind_of(Boolean)
+  array[0].kind_of(Integer) != true ? value = array[0] : nil
   while i < array.length do
     value = yield(array[i], value)
     i += 1
